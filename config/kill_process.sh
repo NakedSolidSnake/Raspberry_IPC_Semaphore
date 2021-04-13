1c90e54e92acdcccf7ba6048c07585ee6bb924a5
Cristiano Silva <cristianosstec@gmail.com>
2021-04-13 13:50:16 -0300

    remove ipc from shell script

--- config/kill_process.sh
+++ config/kill_process.sh
@@ -9,3 +9,9 @@ if [ `pgrep led_process` > 0 ]; then
     echo "Killing led_process"
     kill `pgrep led_process`
 fi
+
+ID=$(ipcs -s | grep `printf 0x"%08x\n" 1234` | awk -F' ' '{print $2}')
+if [ $ID > 0 ]; then 
+    echo "Removing semaphore ID=${ID}"
+    ipcrm -s $ID
+fi
