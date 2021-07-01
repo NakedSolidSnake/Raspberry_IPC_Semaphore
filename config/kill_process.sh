ce5694d77f9c011bf4248542c2b525157dafc80f
solid <cristianosstec@gmail.com>
2021-07-01 14:26:55 -0300

    adjust script

--- config/kill_process.sh
+++ config/kill_process.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ `pgrep button_process` > 0 ]; then
-    echo "Killing button_process"
-    kill `pgrep button_process`
-fi
-
 if [ `pgrep led_process` > 0 ]; then    
     echo "Killing led_process"
     kill `pgrep led_process`
+fi
+
+if [ `pgrep button_process` > 0 ]; then
+    echo "Killing button_process"
+    kill `pgrep button_process`
 fi
 
 ID=$(ipcs -s | grep `printf 0x"%08x\n" 1234` | awk -F' ' '{print $2}')
